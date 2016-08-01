e0f4a4f24384a1026abe53b548bef274d91f8818
Alejandro Peña Florentín <alejandropenaflorentin@gmail.com>
2016-08-01 12:19:19 +0200

    Fixed method checking when looking for a route

--- src/Router.php
+++ src/Router.php
@@ -102,7 +102,7 @@ class Router
         $found = null;
         $index = 0;
         while ($index < count($this->routes) && null === $found) {
-            if ($this->routes[$index]->match($uri)) {
+            if ($this->routes[$index]->match($uri) and in_array(strtolower($method), $this->routes[$index]->getHttpMethods())) {
                 $found = $this->routes[$index];
             }
             $index++;
